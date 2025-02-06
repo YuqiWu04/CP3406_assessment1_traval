0d108f2b25a2871dd86ff09bb9b38840d7ece98a
yukey <yuqi.wu1@my.jcu.au>
2025-02-07 01:24:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cp3406_assessment1_traval/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.cp3406_assessment1_traval
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cp3406_assessment1_traval.ui.theme.CP3406_assessment1_travalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CP3406_assessment1_travalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+                    LogpageImage(modifier = Modifier.padding(paddingValues))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Login(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxHeight().padding(10.dp)) {
+        Text(
+            text = "Login",
+            fontSize = 40.sp,
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email or Username") }, modifier = Modifier.fillMaxWidth()
+
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { password = it },
+            label = { Text("Password") }, modifier = Modifier.fillMaxWidth()
+
+        )
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Login")
+        }
+        Text(
+            text = "Forgot your password? "
+        )
+    }
+
+
+}
+
+@Preview(showBackground = true,name = "Log in page")
+@Composable
+fun UserInput() {
+    CP3406_assessment1_travalTheme {
+        LogpageImage()
+    }
+}
+@Composable
+fun LogpageImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.login)
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 1F
+        )
+        Login()
+    }
+
+}
---
+++ app/src/main/java/com/example/cp3406_assessment1_traval/MainActivity2.kt
@@ -0,0 +1,94 @@
+package com.example.cp3406_assessment1_traval
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cp3406_assessment1_traval.ui.theme.CP3406_assessment1_travalTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CP3406_assessment1_travalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    TravelDisplay(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun TravelDisplay(modifier: Modifier = Modifier) {
+    val items = listOf("8:00am Singapore airline\n     Flight number: CA123", "12:00 check in \n     Location: Marina Bay Sands Hotel", "17:00pm ABC swimming poor\n     Location:Marina Square", "20:00 Watch fireworks\n     Location: Marina Bay Sands Hotel","wwwwww","hhhhhh","6666666", "888888888","yyyyyyyy")
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,verticalArrangement = Arrangement.Top,) {
+        Text(text = "Today's itinerary",
+            fontSize = 30.sp,
+            modifier =modifier ,
+            lineHeight = 28.sp,
+
+
+        )
+        Spacer(modifier = Modifier.height(50.dp))
+
+
+
+        LazyColumn {
+            items(items.size) { index ->
+                Box(
+                    modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(Color.Gray ,shape = RoundedCornerShape(16.dp))
+                        .padding(16.dp)){
+
+                    Text(
+                    text = "⚡\uFE0F${items[index]}",
+                    fontSize = 20.sp,
+                        color = Color.White
+
+                )
+                    }
+
+
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, name = "TravelDisplay")
+@Composable
+fun TravalShow(){
+    CP3406_assessment1_travalTheme {
+        TravelDisplay()
+    }
+}
+
+
+
+
+
+
+
+
